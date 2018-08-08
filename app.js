c9f644bfab4a85b5e9e0a4d939eb611ef3d6a586
sunshan <sunshanbbt>
2018-08-09 00:02:45 +0800

    优化用户授权完善开始界面

--- app.js
+++ app.js
@@ -1,8 +1,64 @@
+// 引入配置文件config.js
+const config = require('/utils/config.js');
 //app.js
 App({
-  globalData: {
-    appid: 'wx78741ebb07a97411',
-    secret: '24640dbeeed1cd07f48509c239d856a1',
-    rdSessionKey: ''
+  onShow:function(){
+    var that = this;
+    console.log('生命周期函数，监听页面显示');
+    wx.getSetting({
+         success: function (res) {
+          if (res.authSetting['scope.userInfo']) {
+            wx.getUserInfo({
+              success: function (res) {
+                //用户已经授权过
+                console.log('用户已经授权');
+                //校验session是否过期
+                that.getCheckSession();
+              }
+            });
+          }
+         }  
+    })
+  },
+  getCheckSession() {
+    var that = this;
+    wx.checkSession({
+      success: function () {
+        console.log('session未过期，暂不做任何业务处理');
+      },
+      fail: function () {
+        console.log('session已过期，正在重新获取..');
+        //code获取成功，保存为当前页面的全局变量code
+        that.getLogin();
+      }
+    })
+  },
+  //获取code
+  getLogin() {
+    //code获取成功，保存为当前页面的全局变量code
+    wx.login({
+      success: res => {
+        if(res.code){
+          console.log('当前用户登录凭证：' + res.code);
+          wx.request({
+            url: config.queryOpenIdUrl,
+            data: { code: res.code },
+            success: function (res) {
+              wx.setStorage({
+                key: 'rdSessionKey',
+                data: res.data.rdSessionKey,
+              });
+              console.log("当前用户服务端返回的rdSessionKey:" + res.data.rdSessionKey);
+
+            }
+          })
+        } else {
+          console.log('获取用户登录态失败！' + res.errMsg)
+        }
+      },
+      fail: res => {
+        console.log(res.err_desc);
+      }
+    })
   }
 })
--- pages/login/login.js
+++ pages/login/login.js
@@ -3,103 +3,10 @@ const config = require('../../utils/config.js');
 var app = getApp();
 Page({
   data: {
-    //判断小程序的API，回调，参数，组件等是否在当前版本可用。
-    canIUse: wx.canIUse('button.open-type.getUserInfo')
-  },
-  onLoad() {
-    var that = this;
-    // 查看是否授权
-    wx.getSetting({
-      success: function (res) {
-        if (res.authSetting['scope.userInfo']) {
-          wx.getUserInfo({
-            success: function (res) {
-              //从数据库获取用户信息
-              that.getCheckSession();
-              //用户已经授权过
-              wx.switchTab({
-                url: '/pages/index/index'
-              })
-            }
-          });
-        }
-      }
-    })
-  },
-  getCheckSession() {
-    var that = this;
-    wx.checkSession({
-      success: function () {
-        that.getLogin();
-      },
-      fail: function () {
-        //code获取成功，保存为当前页面的全局变量code
-        that.getLogin();
-      }
-    })
-  },
-  //获取code
-  getLogin() {
-    //code获取成功，保存为当前页面的全局变量code
-    wx.login({
-      success: res => {
-        if(res.code){
-          console.log('我是登录凭证：' + res.code);
-          var appinfo = app.globalData;
-          wx.request({
-            url: config.queryOpenIdUrl,
-            data: { code: res.code, appid: appinfo.appid, secret: appinfo.secret },
-            success: function (res) {
-              wx.setStorage({
-                key: 'rdSessionKey',
-                data: res.data.rdSessionKey,
-              });
-              console.log("我是服务端返回rdSessionKey:" + res.data.rdSessionKey);
-
-            }
-          })
-        } else {
-          console.log('获取用户登录态失败！' + res.errMsg)
-        }
-      },
-      fail: res => {
-        this.wetoast.toast({ title: res.err_desc });
-      }
-    })
-  },
-  getPhoneNumber(e) {
-    console.log(e.detail.errMsg)
-    console.log(e.detail.iv)
-    console.log(e.detail.encryptedData)
-    // 获取用户的session_key、openID以及isRegister（注册情况）
-    app.globalMethod.GET({
-      url: urlList.getOpenidUrl,
-      data: {
-        code: this.data.code
-      },
-      success: res => {
-        if (res.data.code == '200') {
-          //获取用户的openID
-        } else {
-          this.wetoast.toast({ title: res.data.message });
-        }
-      }
-    })
-  },
-  getUserInfo(e) {
-    this.userinfo = e.detail.userinfo;
-    console.log(userinfo.nickname);
-    if(e.detail.userinfo){
-      
-      wx.showToast({
-        title: '授权成功',
-        icon: 'success',
-        duration: 1500
-      })
-    }
+    loadingHidden: false
   },
   bindGetUserInfo: function (e) {
-    console.log('用户按了允许授权按钮');
+    console.log('用户按了开始游戏按钮');
     console.log('用户昵称' + e.detail.userInfo.nickName);
     if (e.detail.userInfo) {
       //用户按了允许授权按钮
@@ -116,7 +23,12 @@ Page({
           gender: e.detail.userInfo.gender,
           country: e.detail.userInfo.country,
           language: e.detail.userInfo.language,
-          rdSessionKey: app.globalData.rdSessionKey
+          rdSessionKey: wx.getStorage({
+            key: 'rdSessionKey',
+            success: function(res) {
+              storage: res.data;
+            },
+          })
         },
         header: {
           'content-type': 'application/json'
@@ -127,24 +39,14 @@ Page({
           console.log("插入小程序登录用户信息成功！");
         }
       });
-      //授权成功后，跳转进入小程序首页
-      wx.switchTab({
-        url: ''
-      })
+      //授权成功后，跳转进入小程序游戏界面
+      wx.navigateTo({
+        url: '/pages/gaming/gaming'
+      });
     } else {
-      //用户按了拒绝按钮
-      wx.showModal({
-        title: '警告',
-        content: '您点击了拒绝授权，将无法进入小程序，请授权之后再进入!!!',
-        showCancel: false,
-        confirmText: '返回授权',
-        success: function (res) {
-          if (res.confirm) {
-            console.log('用户点击了“返回授权”')
-          }
-        }
-      })
+      console.log('授权失败')
     }
-  },
+  }
+
 
 })
